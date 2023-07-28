b035593704d3caa59202b381b05396db4c08feb3
jagatheeswaran0 <jagatheesrm0@gmail.com>
2023-07-28 23:30:37 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import SignUp from "./Component/SignUp"; 
-import About1 from "./Component/About1"; 
-import SenHeader from "./Component/Whyssenchola/SenHeader"; 
-import Form from './Component/form_page/Form'; 
-import Service from './Component/service_page/ServiceApp'; 
 import React from 'react';
-import Contact from './Component/Contact/Contact';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './Component/LoginPage/Login';
+import About1 from "./Component/About1";
+import Contact from './Component/Contact/Contact';
 import HomePage from './Component/HomePage/Home';
+import Login from './Component/LoginPage/Login';
+import SignUp from "./Component/SignUp";
+import SenHeader from "./Component/Whyssenchola/SenHeader";
+import Form from './Component/form_page/Form';
+import Service from './Component/service_page/ServiceApp';
 import Navbar from './Navbar';
 const App = () =>{
   return(
@@ -18,14 +18,14 @@ const App = () =>{
         <Navbar />
         <main>
           <Routes>
-            <Route path="/senheader"  element={<SenHeader/>} />
+            <Route path="^/senheader"  element={<SenHeader/>} />
             <Route path="/about" element={<About1/>} />
             <Route path="/services" element={<Service/>} />
             <Route path="/form" element={<Form/>} />
             <Route path="/signup" element={<SignUp/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/contact" element={<Contact/>}/>
-            <Route path='/' element={<HomePage/>} exact />
+            <Route path='senchola/' element={<HomePage/>} exact />
           </Routes>
         </main>
       </div>
